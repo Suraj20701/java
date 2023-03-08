4fbe7a7ecb6359db530fe3dbb98fe641e3169af4
Suraj <82625063+Suraj20701@users.noreply.github.com>
2023-03-08 09:12:48 +0530

    Add files via upload

---
+++ TermworkFiveB/Main.java
@@ -0,0 +1,10 @@
+package TermworkFiveB;
+
+public class Main {
+    public static void main(String[] args) {
+        Cuboid cube = new Cuboid(10, 5, 3);
+        cube.computePerimeter();
+        cube.computerArea();
+        cube.computeVolume();
+    }
+}
---
+++ TermworkFiveB/Rectangle.java
@@ -0,0 +1,21 @@
+package TermworkFiveB;
+
+/**
+ * Rectangle
+ */
+public class Rectangle {
+    int length, width;
+    Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+    int computerArea(){
+        return length * width;
+    }
+
+    int computePerimeter(){
+        return 2*(length + width);
+    }
+    
+}
